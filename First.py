ae09d8dbe6466bfd5145ea41bbb820d123ce0e48
Ranarohit123 <85821702+Ranarohit123@users.noreply.github.com>
2021-06-13 13:25:31 +0530

    Add files via upload

---
+++ First.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 13 13:14:44 2021
+
+@author: RAHUL
+"""
+
+x=[1,2,3,4,5]
+y=[2,4,6,8,10]
+print(x)
+print(y)
+import matplotlib.pyplot as hm
+hm.plot(x,y)
